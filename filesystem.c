f39b9eac63b4b570fdc7a415cb4b19af2a4cf516
SammySchool <sgood11@lsu.edu>
2023-11-28 17:52:54 -0600

    new

--- filesystem.c
+++ filesystem.c
@@ -1,10 +1,121 @@
 #include "filesystem.h"
 #include "softwaredisk.h"
+#include "softwaredisk.c"
+#include <string.h>
+#include <stdio.h>
+#include <stdint.h>
 
+#define NUM_BITMAP_BLOCKS 2
+#define NUM_INODE_BLOCKS 4
+#define NUM_DIR_BLOCKS 64
 
+#define MAX_FILES 512
+#define DATA_BITMAP_BLOCK 0
+#define INODE_BITMAP_BLOCK 1
+#define FIRST_INODE_BLOCK 2
+#define LAST_INODE_BLOCK 5
 
+#define INODES_PER_BLOCK 128
+#define FIRST_DIR_ENTRY_BLOCK 6
+#define LAST_DIR_ENTRY_BLOCK 69
+#define DIR_ENTRIES_PER_BLOCK 8
 
+#define FIRST_DATA_BLOCK 70
+#define LAST_DATA_BLOCK 4095
+#define MAX_FILENAME_SIZE 507
+#define NUM_DIRECT_INODE_BLOCKS 13
+#define NUM_SINGLE_INDIRECT_BLOCKS (SOFTWARE_DISK_BLOCK_SIZE / sizeof(uint16_t))
 
+#define MAX_FILE_SIZE (NUM_DIRECT_INODE_BLOCKS + NUM_SINGLE_INDIRECT_BLOCKS) * SOFTWARE_DISK_BLOCK_SIZE
+
+#define STARTING_SPACE 4026
+
+uint8_t buf[SOFTWARE_DISK_BLOCK_SIZE];
+uint8_t data_bitmap[SOFTWARE_DISK_BLOCK_SIZE];
+uint8_t inode_bitmap[SOFTWARE_DISK_BLOCK_SIZE];
+
+// typedef struct DataBlock{
+//     char data[1];
+// }DataBlock;
+
+// typedef struct Data_Blocks{
+//     char block[STARTING_SPACE];
+// }Data_Blocks;
+
+// Data_Blocks filesystem;
+uint8_t filesystem[STARTING_SPACE] = {0};
+FSError fserror;
+
+typedef struct Inode{
+    uint16_t direct_addresses[NUM_DIRECT_INODE_BLOCKS]; //26 bytes
+    uint16_t indirect;             //2 bytes
+    uint32_t size;                   //4 bytes
+                                         //26+2+4 = 32 bytes
+}Inode;
+
+typedef struct Inode_Block{
+
+    Inode IBlock[INODES_PER_BLOCK]; //32 * 128 = 4096
+
+}Inode_Block;
+
+typedef struct Dir_Address{
+
+    uint8_t name[MAX_FILENAME_SIZE];  //507 bytes
+    uint16_t id;        //2 bytes
+    uint8_t mode;       //1 byte
+    uint16_t NT;      //2 bytes
+                        //507+2+1+2 = 512
+}Dir_Address;
+
+typedef struct Dir_Block{
+
+    Dir_Address dblock[DIR_ENTRIES_PER_BLOCK]; //512 * 8 = 4096
+
+}Dir_Block;
+
+typedef struct FileInternals{
+    char *fname;
+    uint32_t size;
+    FileMode mode;
+    unsigned int cursor_position;
+    Inode location;
+}FileInternals;
+
+static uint16_t find_free_inode(void)
+{
+    for(int i = 0; i < 512; i++)
+    {
+        if(inode_bitmap[0] == 0)
+        {
+            return i;
+        }
+    }
+    return INT16_MAX;
+}
+
+static uint32_t find_free_block(void)
+{
+    for(uint32_t i = 70; i < SOFTWARE_DISK_BLOCK_SIZE; i++)
+    {
+        if (data_bitmap[i] = 0){
+            return i;
+        }
+    }
+    return INT32_MAX;
+}
+
+static int mark_inode(uint16_t idx)
+{
+    inode_bitmap[idx] = 1;
+    return 1;
+}
+
+static int mark_block(uint16_t idx)  
+{
+    data_bitmap[idx] = 1;
+    return 1; 
+}
 
 // open existing file with pathname 'name' and access mode 'mode'.
 // Current file position is set to byte 0.  Returns NULL on
@@ -17,12 +128,20 @@ File open_file(char *name, FileMode mode){
 // mode READ_WRITE.  Current file position is set to byte 0.  Returns
 // NULL on error. Always sets 'fserror' global.
 File create_file(char *name){
+    fserror = FS_NONE;
+    File file;
+    //file->fname = name;
+    
+    //find unused Inode
+    //find unused Data Block
+    //reserve Inode and Data Block
 
 }
 
 // close 'file'.  Always sets 'fserror' global.
 void close_file(File file){
-
+    fserror = FS_NONE;
+    
 }
 
 // read at most 'numbytes' of data from 'file' into 'buf', starting at the 
--- formatfs.c
+++ formatfs.c
@@ -1,115 +1,39 @@
 #include "filesystem.h"
+#include "filesystem.c"
 #include "softwaredisk.h"
+#include "softwaredisk.c"
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
-#define SHORT_MAX 65535
 
-//char block[SOFTWARE_DISK_BLOCK_SIZE];
-char buf[SOFTWARE_DISK_BLOCK_SIZE];
-unsigned char data_bitmap[4096];
-unsigned char inode_bitmap[512];
 
-typedef struct Inode{
-    unsigned short direct_addresses[13]; //26 bytes
-    unsigned short indirect;             //2 bytes
-    int size;                   //4 bytes
-                                         //26+2+4 = 32 bytes
-}Inode;
-
-typedef struct Inode_Block{
-
-    Inode IBlock[128]; //32 * 128 = 4096
-
-}Inode_Block;
-
-typedef struct Dir_Address{
-
-    unsigned char name[507];  //507 bytes
-    unsigned short id;        //2 bytes
-    unsigned char mode;       //1 byte
-    unsigned char NT[2];      //2 bytes
-                              //507+2+1+2 = 512
-}Dir_Address;
-
-typedef struct Dir_Block{
-
-    Dir_Address dblock[8]; //512 * 8 = 4096
-
-}Dir_Block;
-
-void initialize_bitmap()
-{
+int check_structure_alignment(void){
+    printf("Expecting sizeof(Inode) = 32, actual = %lu\n", sizeof(Inode));
+    //printf("Expecting sizeof(IndirectBlock) = 32, actual = %lu\n", sizeof(Inode));
+    printf("Expecting sizeof(Inode_Block) = %d, actual = %lu\n", SOFTWARE_DISK_BLOCK_SIZE,sizeof(Inode_Block));
+    printf("Expecting sizeof(Dir_Address) = 32, actual = %lu\n", sizeof(Dir_Address));
+    printf("Expecting sizeof(Dir_Block) = %d, actual = %lu\n", SOFTWARE_DISK_BLOCK_SIZE, sizeof(Dir_Block));
+    printf("Expecting sizeof(data_bitmap) = %d, actual = %lu\n", SOFTWARE_DISK_BLOCK_SIZE,sizeof(data_bitmap));
     
-    for(int i = 0; i < 4096; i++){
-        data_bitmap[i] = 0;
+    if(sizeof(Inode) != 32|| 
+    sizeof(Inode_Block) != SOFTWARE_DISK_BLOCK_SIZE || 
+    sizeof(Dir_Address) != 512 || 
+    sizeof(Dir_Block) != SOFTWARE_DISK_BLOCK_SIZE || 
+    sizeof(data_bitmap) != SOFTWARE_DISK_BLOCK_SIZE){
+    return 0;
     }
-    for(int i = 0; i < 512; i++){
-        inode_bitmap[i] = 0;
-    }
-    memcpy(buf, data_bitmap, sizeof(data_bitmap));
-    write_sd_block(buf, 0);
-    memcpy(buf, inode_bitmap, sizeof(inode_bitmap));
-    memset(buf+sizeof(data_bitmap), '0', SOFTWARE_DISK_BLOCK_SIZE - sizeof(inode_bitmap));
-    write_sd_block(inode_bitmap, 1);
-    printf("Contents of block # 3:\n");
-  for (int j=0; j < SOFTWARE_DISK_BLOCK_SIZE; j++) {
-    printf("%c", buf[j]);
-  }
-    
-}
-
-void initialize_inode_blocks(){
-    Inode_Block nodeBlock[4];
-    int blockNum = 2;
-
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < 128; i++){
-            for(int r = 0; r < 13; r++){
-            nodeBlock[i].IBlock[j].direct_addresses[r] = SHORT_MAX;
-            }
-        
-        nodeBlock[i].IBlock[j].indirect = SHORT_MAX;
-        nodeBlock[i].IBlock[j].size = -1;
-        }
-            write_sd_block(nodeBlock[i].IBlock, blockNum);
-            blockNum++;
-    }
-
-}
-
-void initialize_dir_blocks(){
-    Dir_Block dirBlocks[64];
-    int blockNum = 6;
-    for(int i = 0; i < 64; i++){
-        for(int j = 0; j < 8; j++){
-            for(int r = 0; r < 507; r++){
-                dirBlocks[i].dblock[j].name[r] = ' ';//define null
-            }
-            dirBlocks[i].dblock[j].id = SHORT_MAX;
-            dirBlocks[i].dblock[j].mode = ' ';
-            dirBlocks[i].dblock[j].NT[0] = '\\';//prolly wrong
-            dirBlocks[i].dblock[j].NT[1] = '0';
-        }
-        write_sd_block(dirBlocks[i].dblock, blockNum);
-        blockNum++;
-    }
+    return 1;
 }
 
 int main(int argc, char *argv[]){
 
 init_software_disk();
-initialize_bitmap();
-initialize_inode_blocks();
-initialize_dir_blocks();
-return 0;
+if(!check_structure_alignment)
+{
+    return 0;
+}
 
+return 1;
 
-
-
-// int num_reserved_blocks = NUM_BITMAP_BLOCKS + NUM_INODE_BLOCKS + NUM_DIR_BLOCKS;
-
-// for (i = )
-
-//return 1;
 }
